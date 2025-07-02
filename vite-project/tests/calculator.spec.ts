ca44d99351ff58cc8a8bf22ec97cbcc67bc56e8a
Sonali Elabada Arachchige <sonali.elabada-arachchige@stud.th-deg.de>
2025-07-02 08:17:45 +0200

    moved file

--- vite-project/tests/calculator.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("anonymous calculator works", async ({ page }) => {
-  await page.goto("http://localhost:8080");
-  await page.getByRole("button", { name: "Survival Calculator" }).click();
-  await page.selectOption("select[name=model]", { label: "RandomForest" });
-  await page.fill("input[name=Age]", "35");
-  await expect(page.getByTestId("prediction-result")).toHaveText(/Survived|Dead/);
-});
